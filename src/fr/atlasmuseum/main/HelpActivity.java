3b963a36700d614497abe55d0d3c3329ea1a8bca
bodin884@gmail.com <bodin884@gmail.com@571493e5-6e2c-7360-0832-9dfcab19a3e2>
2014-09-01 13:35:46 +0000

    added

---
+++ src/fr/atlasmuseum/main/HelpActivity.java
@@ -0,0 +1,43 @@
+package fr.atlasmuseum.main;
+
+import fr.atlasmuseum.R;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class HelpActivity extends Activity{
+
+	private static final String DEBUG_TAG = "UnPourCent/HelpActivity";
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_layout);
+        Log.d(DEBUG_TAG, "onCreate");
+        
+        addListenerOnScreen();
+
+    }
+    public void addListenerOnScreen() {
+    	
+		final Context context = this;
+		Button icon = null;
+		
+		icon = (Button) findViewById(R.id.clickscreen2);
+ 
+		icon.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+			    Intent intent = new Intent(context, MainActivity.class);
+                            startActivity(intent);   
+			}
+ 
+		});
+ 
+	}
+}
