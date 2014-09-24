d261acdff835eddda876070cfea4a8814847b267
eric.courtois@gmail.com <eric.courtois@gmail.com@571493e5-6e2c-7360-0832-9dfcab19a3e2>
2014-09-24 09:57:30 +0000

    Clean up LoaderActivity

--- branches/refactoring/src/fr/atlasmuseum/main/LoaderActivity.java
+++ branches/refactoring/src/fr/atlasmuseum/main/LoaderActivity.java
@@ -1,10 +1,8 @@
 package fr.atlasmuseum.main;
 
-
 import fr.atlasmuseum.R;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
@@ -12,9 +10,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.location.LocationManager;
@@ -24,84 +20,75 @@ import android.location.LocationManager;
  *
  */
 public class LoaderActivity extends Activity  {
-	private static final String DEBUG_TAG = "AtlasMuseum/Loader";
-	static final String NB_ENTRIES = "nbentries";
+	private static final String DEBUG_TAG = "AtlasMuseum/LoaderActivity";
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loader);
         Log.d(DEBUG_TAG, "onCreate");
         
-        addListenerOnScreen();
-       // getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         getActionBar().hide();
-        TextView titre = (TextView) findViewById(R.id.app_name);
+
+        Typeface fontRegular = Typeface.createFromAsset(getAssets(), "RobotoCondensed-Regular.ttf");
+        Typeface fontBold = Typeface.createFromAsset(getAssets(), "RobotoCondensed-Bold.ttf");
+
+        TextView textTitre = (TextView) findViewById(R.id.text_title);
+        textTitre.setTypeface(fontBold);
+        
+        TextView textIntro1 = (TextView)findViewById(R.id.text_intro1);
+        textIntro1.setTypeface(fontRegular);
+
         TextView text_intro2 = (TextView)findViewById(R.id.text_intro2);
-        TextView text_intro1 = (TextView)findViewById(R.id.text_intro1);
-        Typeface font1 = Typeface.createFromAsset(getAssets(), "RobotoCondensed-Bold.ttf");
-        Typeface font = Typeface.createFromAsset(getAssets(), "RobotoCondensed-Regular.ttf");
-        titre.setTypeface(font1);
-        text_intro2.setTypeface(font);
-        text_intro1.setTypeface(font);
+        text_intro2.setTypeface(fontRegular);
         
 		try {
 			PackageInfo pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-	        TextView text_version = (TextView)findViewById(R.id.text_version);
-	        text_version.setHint(pinfo.versionName);
+	        TextView textVersion = (TextView)findViewById(R.id.text_version);
+	        textVersion.setHint(pinfo.versionName);
 		} catch (NameNotFoundException e) {
 		}
         
+		
         
-      //check if GPS is activated
+		//check if GPS is activated
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             Toast.makeText(this, R.string.GPSactivatedmes, Toast.LENGTH_SHORT).show();
-        }else
-        {
+        }
+        else {
             showGPSDisabledAlertToUser();
         }
-        
     }
-    public void addListenerOnScreen() {
-    	
-		final Context context = this;
-		Button icon = null;
-		
-		icon = (Button) findViewById(R.id.clickscreen);
- 
-		icon.setOnClickListener(new OnClickListener() {
- 
-			@Override
-			public void onClick(View arg0) {
-			    Intent intent = new Intent(context, MainActivity.class);
-                            startActivity(intent);
-                            finish();
-			}
 
-		});
- 
-	}
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        super.dispatchTouchEvent(ev);
+		Intent intent = new Intent(LoaderActivity.this, MainActivity.class);
+		startActivity(intent);
+		finish();
+		return true;
+    }
     
-
     private void showGPSDisabledAlertToUser(){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage(getResources().getString(R.string.gps_click_to_enable))
-        .setCancelable(false)
-        .setPositiveButton(getResources().getString(R.string.gsp_link_to_enable),
-                new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int id){
-                Intent callGPSSettingIntent = new Intent(
-                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(callGPSSettingIntent);
-            }
-        });
-        alertDialogBuilder.setNegativeButton(getResources().getString(R.string.annuler),
-                new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int id){
-                dialog.cancel();
-            }
-        });
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
+    	AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+    	alertDialogBuilder.setMessage(getResources().getString(R.string.gps_click_to_enable));
+    	alertDialogBuilder.setCancelable(false);
+		alertDialogBuilder.setPositiveButton(
+				getResources().getString(R.string.gsp_link_to_enable),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						startActivity(callGPSSettingIntent);
+					}
+				});
+		alertDialogBuilder.setNegativeButton(
+				getResources().getString(R.string.annuler),
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+    		}
+    	});
+    	AlertDialog alert = alertDialogBuilder.create();
+    	alert.show();
     }
 }
